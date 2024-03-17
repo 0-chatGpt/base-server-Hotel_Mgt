5f595a3c1c7c5e9794a45a23fc9ca821115a6c7a
Chinemeremun Okereke <goldenEye56.me@gmail.com>
2024-03-17 14:06:13 +0100

    setting up database connection

---
+++ database/database.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const mongoose = require("mongoose");
+// const cors = require("cors");
+
+async function db(remote=false){
+    try {
+        const conn = remote ? await mongoose.connect(process.env.DATABASE_URL) : await mongoose.connect(process.env.DATABASE_URL_LOCAL, {family:4});
+        console.log(`Database connected: ${conn.connection.host}`);
+        return true;
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+}
+
+module.exports =  db;
