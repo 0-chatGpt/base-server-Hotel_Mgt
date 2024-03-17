9f18d25023a85fbc4fb0c0faf29a56f107fcc7c3
Chinemeremun Okereke <goldenEye56.me@gmail.com>
2024-03-17 15:27:49 +0100

    constructing controller

--- app.js
+++ app.js
@@ -1,1 +1,78 @@
 "use strict";
+
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Controller = require("./controllers/controller.js");
+const db = require("./database/database.js");
+const cors = require("cors");
+mongoose.set('strictQuery', false);
+
+const app = express();
+const baseUrl = `http://localhost:${process.env.PORT}`
+
+// Mongodb connect
+db(!true);
+const controller = new Controller();
+// Using middlewares
+app.use(cors);
+
+
+// filters
+let searchRoomMaximumPriceMatch;
+let searchRoomMinimumPriceMatch;
+let searchRoomNameMatch;
+let searchRoomTypeNameMatch;
+
+/** client get request  */
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Hello World</h1>`)
+});
+
+app.get(`${baseUrl}/api/v1/rooms-types`, controller.getAllRoomTypes);
+
+app.get(`${baseUrl}/api/v1/rooms?search=${searchRoomNameMatch}&roomType=${searchRoomTypeNameMatch}&minPrice=${searchRoomMinimumPriceMatch}&maxPrice=${searchRoomMaximumPriceMatch}`,controller.roomSelector);
+
+app.get(`${baseUrl}/api/v1/rooms/:roomId`,controller.roomSelector);
+
+
+/** end  */
+
+/** client post request  */
+
+
+app.post(`${baseUrl}/api/v1/rooms-types`, controller.createRoomType);
+
+app.post(`${baseUrl}/api/v1/rooms`,controller.createSomeRoom);
+
+
+/** end  */
+
+/** Other methods */
+
+app.patch(`{${baseUrl}}/api/v1/rooms/:roomId`, controller.roomEdit);
+
+app.delete(`{${baseUrl}}/api/v1/rooms/:roomId`, controller.roomTrash);
+
+/** end */
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Example app listening on port ${process.env.PORT}`);
+})
+
+/**
+ * function rin(){
+ * 
+ * }
+ * 
+ * const rit = function (){
+ * 
+ * }
+ * 
+ * const ad = () => {
+ * 
+ * }
+ * 
+ */
+
--- controllers/controller.js
+++ controllers/controller.js
@@ -5,24 +5,31 @@ const mongoose = require("mongoose");
 
 
 
-
 class Controller {
-    async getAllRoomTypes (){
+    async getAllRoomTypes (req, res){
+        const roomTypes = await roomType.find();
+        res.send(JSON.stringify(roomTypes));
+    }
+    async createRoomType (req, res){
+
+        const _types = new roomType(req.body);
+        await _types.save();
+    }
+    async createSomeRoom (req, res){
         return await new Promise()// DB work
     }
-    async createRoomType (){
+    async roomSelector (req, res){
+        // const _room = await room.findById()
         return await new Promise()// DB work
     }
-    async createSomeRoom (){
+    async roomEdit (req, res){
+        
         return await new Promise()// DB work
     }
-    async roomSelector (){
-        return await new Promise()// DB work
-    }
-    async roomEdit (){
-        return await new Promise()// DB work
-    }
-    async roomTrash (){
+    async roomTrash (req, res){
         return await new Promise()// DB work
     }
 }
+
+
+module.exports = Controller;
