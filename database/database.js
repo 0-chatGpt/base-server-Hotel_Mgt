4b98b54dff27a191f10bfffe66c86253cdeec9cd
Chinemeremun Okereke <goldenEye56.me@gmail.com>
2024-03-30 09:07:03 +0100

    .

--- database/database.js
+++ database/database.js
@@ -3,9 +3,9 @@
 const mongoose = require("mongoose");
 // const cors = require("cors");
 
-async function db(remote=false){
+async function db(remote=true){
     try {
-        console.log(`${process.env.DATABASE_URL}`);
+        // console.log(`${process.env.DATABASE_URL}`);
         const conn = remote ? await mongoose.connect(`${process.env.DATABASE_URL}`) : await mongoose.connect(`${process.env.DATABASE_URL_LOCAL}`, {family:4});
         console.log(`Database connected: ${conn.connection.host}`);
         return true;
